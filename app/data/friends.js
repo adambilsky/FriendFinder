2008a628d327611b3110de3f6e9893f008cf62dc
Adam Bilsky <adambilsky@gmail.com>
2018-08-28 17:48:28 -0500

    fixed friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,26 @@
-[
-{
-    "name": "",
-    "photo": "",
-    "scores": []
-}
+var friends =
+    [
+        {
+            "name": "Bilbo Baggins",
+            "photo": "https://apilgriminnarnia.files.wordpress.com/2012/12/bilbo-baggins-2012-hobbit-pipe.jpeg",
+            "scores":
+                [
+                    5,
+                    3,
+                    1,
+                    1,
+                    1,
+                    5,
+                    3,
+                    5,
+                    3,
+                    5
+                ]
+        }
 
 
 
 
-]
+    ]
+
+module.exports = friends;
